46cc0ca518bba6eed1dc30e607fc4472fed252af
lily <lily@iguiyu.com>
2016-09-23 16:11:27 +0800

    d

--- mall/src/main/java/com.lll.demo/Client.java
+++ mall/src/main/java/com.lll.demo/Client.java
@@ -18,24 +18,24 @@ import io.netty.channel.ChannelPipeline;
 public class Client {
 
     public static void main(String args[]) {
-        // Server服务启动器
-        ServerBootstrap bootstrap = new ServerBootstrap(
-                new NioServerSocketChannelFactory(
-                        Executors.newCachedThreadPool(),
-                        Executors.newCachedThreadPool()));
-        // 设置一个处理客户端消息和各种消息事件的类(Handler)
-        bootstrap
-                .setPipelineFactory(new ChannelPipelineFactory() {
-                    @Override
-                    public ChannelPipeline.getPipeline()
-                            throws Exception {
-                        return Channels
-                                .pipeline(new HelloServerHandler());
-                    }
-                });
-        // 开放8000端口供客户端访问。
-        bootstrap.bind(new ChannelFuture(8000) {
-        });
+//        // Server服务启动器
+//        ServerBootstrap bootstrap = new ServerBootstrap(
+//                new NioServerSocketChannelFactory(
+//                        Executors.newCachedThreadPool(),
+//                        Executors.newCachedThreadPool()));
+//        // 设置一个处理客户端消息和各种消息事件的类(Handler)
+//        bootstrap
+//                .setPipelineFactory(new ChannelPipelineFactory() {
+//                    @Override
+//                    public ChannelPipeline.getPipeline()
+//                            throws Exception {
+//                        return Channels
+//                                .pipeline(new HelloServerHandler());
+//                    }
+//                });
+//        // 开放8000端口供客户端访问。
+//        bootstrap.bind(new ChannelFuture(8000) {
+//        });
     }
 
     private static class HelloServerHandler extends
